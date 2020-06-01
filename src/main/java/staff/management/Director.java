c563e09cdd9afa809bc2735aadea02fa22fd2eb1
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-01 17:54:09 +0100

    Create Director subclass in management + its constructor

---
+++ src/main/java/staff/management/Director.java
@@ -0,0 +1,8 @@
+package staff.management;
+
+public class Director extends Manager {
+
+    public Director(String name, String nIN, double salary, String deptName) {
+        super(name, nIN, salary, deptName);
+    }
+}
