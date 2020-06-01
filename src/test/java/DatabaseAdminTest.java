132e356abdbdb1bdfb914e8ad35de2180ca2637c
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-01 17:52:26 +0100

    Test DatabaseAdminTest, all test pass

---
+++ src/test/java/DatabaseAdminTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void setUp() {
+        databaseAdmin = new DatabaseAdmin("Fra", "zzz123", 100000.00 );
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Fra", databaseAdmin.getName() );
+    }
+
+    @Test
+    public void canGetNIN() {
+        assertEquals("zzz123", databaseAdmin.getNIN());
+    }
+
+    @Test
+    public void canGetSalary() {
+        assertEquals(100000.00, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(1000.00, databaseAdmin.payBonus(), 0.01);
+    }
+
+    @Test
+    public void canHaveAPayRise() {
+        databaseAdmin.raiseSalary(databaseAdmin.payBonus());
+        assertEquals(101000.00, databaseAdmin.getSalary(), 0.01);
+    }
+
+}
