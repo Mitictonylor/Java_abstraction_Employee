e2573950082f99f74ebc3921b01144608133fd67
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-01 17:12:14 +0100

    test getName(), test pass

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,22 @@
+import Staff.management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void setUp(){
+        manager = new Manager("Francesco", "abc123", 90000.00,"production" );
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Francesco", manager.getName());
+    }
+}
